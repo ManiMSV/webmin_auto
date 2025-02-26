5daa8a1128178667afb8764d306c463f154e0341
MANIMSV <manikandan.2002121@srec.ac.in>
2025-02-26 19:38:09 +0530

    Refactor: Update login functionality to use Robocorp's browser automation and streamline steps

--- features/demo.feature
+++ features/demo.feature
@@ -1,5 +1,4 @@
 Feature: login functionality
   Scenario: login with valid credentials
     Given open_browser_and_navigate_to_login page
-    When enter_username_and_password
-
+    When enter_username_and_password do
--- steps/demo.py
+++ steps/demo.py
@@ -1,19 +1,13 @@
-from behave import given, when, then
-from RPA.Browser.Selenium import Selenium
+from robocorp import browser, vault
+from robocorp import task
 
-@given(u'open_browser_and_navigate_to_login page')
-def open_browser_and_navigate_to_login_page(context):
-    context.browser = Selenium()
-    context.browser.open_browser("https://www.selenium.dev/selenium/web/web-form.html", browser="chrome")
-    
-    title = context.browser.get_title()
-    assert title == "Web form", f"Expected title 'Web Form', but got '{title}'"
-    print("Browser opened and navigated to login page successfully!")
+@task
+def automate_broser():
+    browser.configure(browser_engine="chrome",headless=True)
+    browser.goto("https://www.selenium.dev/selenium/web/web-form.html")
+    _login()
 
-@when(u'enter_username_and_password')
-def enter_username_and_password(context):
-    context.browser.input_text('xpath=//*[@id="my-text-id"]', "mytext")
-    context.browser.input_text('xpath=/html/body/main/div/form/div/div[1]/label[2]/input', "my-password")
-    context.browser.click_button("xpath=/html/body/main/div/form/div/div[2]/button")
-    print("Entered username and password.")
+def _login():
+    page = browser.page()
+    page.click("/html/body/main/div/form/div/div[2]/button")
 
